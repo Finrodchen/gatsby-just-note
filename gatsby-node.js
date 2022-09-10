609aab3eb4889252a50abcf1205fbae4a89f9c8e
finrodchen <s02260441@gmail.com>
2022-09-11 04:51:52 +0800

    fix node.js error

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,32 +6,25 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   // Define a template for blog post
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const tagTemplate = path.resolve("src/templates/tags.js")
 
-
-  const result = await graphql(`
-  {
-    postsRemark: allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      limit: 2000
-    ) {
-        nodes {
-          fields {
-            slug
-          }
-          frontmatter {
-            tags
+  // Get all markdown blog posts sorted by date
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: ASC }
+          limit: 1000
+        ) {
+          nodes {
+            id
+            fields {
+              slug
+            }
           }
         }
       }
-    }
-    tagsGroup: allMarkdownRemark(limit: 2000) {
-      group(field: frontmatter___tags) {
-        fieldValue
-      }
-    }
-  }
-`)
+    `
+  )
 
   if (result.errors) {
     reporter.panicOnBuild(
@@ -63,20 +56,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       })
     })
   }
-
-  // Extract tag data from query
-  const tags = result.data.tagsGroup.group
-
-  // Make tag pages
-  tags.forEach(tag => {
-    createPage({
-      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
-      component: tagTemplate,
-      context: {
-        tag: tag.fieldValue,
-      },
-    })
-  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
